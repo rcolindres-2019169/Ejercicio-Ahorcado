08485f6ed9b3ca541eac5ae1de4a5e2146d1f92c
rcolindres-2019169 <informatica@alumno.local>
2024-01-19 09:07:01 -0600

    Cuarto commit

--- Ahorcado 2024/script.js
+++ Ahorcado 2024/script.js
@@ -1,21 +1,31 @@
 const palabrasOrdenar  = ['computadora', 'html'];
-
+var aleatorio =  [Math.floor(Math.random() * palabra.length)];
 
 function linea1(){
-var c = document.getElementById("panel");
+const c = document.getElementById("panel");
 var ctx = c.getContext("2d");
 ctx.moveTo(300, 0);
 ctx.lineTo(300, 100);
 ctx.stroke();
-}
+};
 
 function cabeza(){ 
-var c = document.getElementById("panel");
+const c = document.getElementById("panel");
 var ctx = c.getContext("2d");
 ctx.beginPath();
 ctx.arc(195, 90, 70, 0, 2 * Math.PI);
 ctx.stroke();
+};
+
+function cuerpo(){
+    var c = document.getElementById("panel");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(400, 0);
+    ctx.lineTo(300, 190);
+    ctx.stroke();
 }
+
 
 linea1();
 cabeza();
+cuerpo();   
