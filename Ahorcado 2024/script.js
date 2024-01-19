b250f4ce22aca9b2d20406d67b85531deae80648
rcolindres-2019169 <informatica@alumno.local>
2024-01-19 08:04:34 -0600

    Segundo push

--- Ahorcado 2024/script.js
+++ Ahorcado 2024/script.js
@@ -1,2 +1,21 @@
 const palabrasOrdenar  = ['computadora', 'html'];
 
+
+function linea1(){
+var c = document.getElementById("panel");
+var ctx = c.getContext("2d");
+ctx.moveTo(500, 1000);
+ctx.lineTo(500, 1000);
+ctx.stroke();
+}
+
+function cabeza(){ 
+var c = document.getElementById("panel");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+}
+
+linea1();
+cabeza();
